869de6b1033d2fd9781ac6d3481e69256fbd1352
Rodrigo de Oliveira Soares <rodrigo.o.soares@hotmail.com>
2018-09-02 18:37:13 -0300

    Código completo

---
+++ LayoutsProject/LayoutsProject/LayoutsProject/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutsProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToStackLayout_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StackLayoutFolder.StackLayoutPage());
+        }
+        private void ToAbsoluteLayout_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AbsoluteLayoutFolder.AbsoluteLayoutPage());
+        }
+        private void ToRelativeLayout_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RelativeLayoutFolder.RelativeLayoutPage());
+        }
+        private void ToGridLayout_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GridFolder.GridLayoutPage());
+        }
+    }
+}
